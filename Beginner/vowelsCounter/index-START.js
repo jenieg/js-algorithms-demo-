bb3e5d6b77bf6968a95577b6a349836dc97e49ae
Jennifer <jmarthagodfrey@gmail.com>
2024-05-28 14:44:18 -0600

    regex, charmap

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,28 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
-
+//Iterative Approach
+const vowels = ["a", "e", "i", "o", "u"]
 function vowelsCounter(text) {
-    // Code goes here
+    let counter = 0;
+    for (let letter of text.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            counter++
+        }
+    }
+    return counter
 }
 
+
+//Regular Expressions(REGEX)
+function vowelsCounter(text) {
+    let matchingInstances = text.match(/[aeiou]/gi);
+    if (matchingInstances) {
+        return matchingInstances.length
+    } else {
+        return 0
+    }
+}
 
 
 module.exports = vowelsCounter;
