1650c1d01dd65e4931b06d48309f4ae3449f1181
Jennifer <jmarthagodfrey@gmail.com>
2024-05-29 11:39:02 -0600

    optimized loop

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,13 +3,33 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
 function palindromeChecker(text) {
-    // Code goes here
+
+    var reversedText = text.toLowerCase()
+        .split('').reverse().join('')
+
+    return text === reversedText
 }
 
+//looping through and comparing characters
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
 
+    let result = charArray.every((letter, index) => {
+        return letter === charArray[charArray.length - index - 1];
+    })
 
+    return result
+}
+
+//optimized
+function palindromeChecker(text) {
+    var textLen = text.length;
+    for (var i = 0; i < textLen / 2; i++) {
+        if (text[i] !== text[textLen - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
 module.exports = palindromeChecker;
