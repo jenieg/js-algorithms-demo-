75475ebf0d79f46b0f2d285aa01bc473be198511
Jennifer <jmarthagodfrey@gmail.com>
2024-05-30 12:38:27 -0600

    imparitive

--- Beginner/pigLatin/index-START.js
+++ Beginner/pigLatin/index-START.js
@@ -9,8 +9,37 @@ Translate the provided string to pig latin by following the rules below:
   - "explain" = "explainway”
 */
 
+//imperative
 function pigLatin(str) {
-  // Code goes here
-}
+  // Convert string to lowercase
+  str = str.toLowerCase();
+  // Initialize array of vowels
+  const vowels = ["a", "e", "i", "o", "u"];
+  // Initialize vowel index to 0
+  let vowelIndex = 0;
 
+  if (vowels.includes(str[0])) {
+    // If first letter is a vowel
+    return str + "way";
+  } else {
+    // If the first letter isn't a vowel i.e is a consonant
+    for (let char of str) {
+      // Loop through until the first vowel is found
+      if (vowels.includes(char)) {
+        // Store the index at which the first vowel exists
+        vowelIndex = str.indexOf(char);
+        break;
+      }
+    }
+    // Compose final string
+    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+  }
+}
+//declarative replace and regex
+function pigLatin(str) {
+  return str
+  .replace(/^([aeiouy])(.*)/, '$1$2way')
+  .replace(/^([^aeiouy]+)(.*)/, '$2$1ay');
+}
+s
 module.exports = pigLatin;
