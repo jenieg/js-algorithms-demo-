1e4bae277f62361de59d9d98a855ea7b73b7b927
Jennifer <jmarthagodfrey@gmail.com>
2024-05-31 12:57:46 -0600

    .slice

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -1,11 +1,62 @@
 /* 
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
+Given two or more arrays, write a function that combines
+their elements into one array without any repetition. 
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// LOOPING THROUGH THE ARRAY
+
 function chunkArray(array, size) {
-    // Code goes here
+    let result = []
+
+    for (value of array){
+
+        let lastArray = result[result.length -1 ]
+        if(!lastArray || lastArray.length == size){
+            result.push([value])
+        }else{
+            lastArray.push(value)
+        }
+    }
+
+    return result
+}
+
+// LOOPING THROUGH THE NUMBER OF CHUNKS
+
+function chunkArray(array, size) {
+    let result = []
+
+    let arrayCopy = [...array]
+
+    while (arrayCopy.length > 0) {
+        result.push(arrayCopy.splice(0, size))
+    }
+
+    return result
+}
+
+
+// USING .SLICE()
+
+function chunkArray(array, size) {
+    let result = []
+
+    for (i = 0; i < array.length; i += size) {
+        let chunk = array.slice(i, i + size)
+        result.push(chunk)
+    }
+
+    return result
+}
+
+//RECURSION
+
+function chunkArray(array, size) {
+    if(array.length <= size){
+        return [array]
+    }
+    return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
 }
 
 
