cfc1631d57ffb3dc40e9bf5477eb601050d3f264
Jennifer <jmarthagodfrey@gmail.com>
2024-05-31 13:12:17 -0600

    for then filter

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,9 +5,26 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+//For Loop
 function falsyBouncer(array) {
-    // Code goes here
+    let result =[]
+    
+    for (value of array){
+        if(value){
+            result.push(value)
+        }
+    }
+
+    return result
+}
+
+
+
+//Filter
+function falsyBouncer(array) {
+    return array.filter((value) => {
+        return Boolean(value)  
+    })
 }
 
 
