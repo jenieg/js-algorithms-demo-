a51309a4804b29072fc585fc3e5666af805f00b2
Jennifer <jmarthagodfrey@gmail.com>
2024-05-31 13:06:43 -0600

    filter than set

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -1,21 +1,50 @@
 /*
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
+Given two or more arrays, write a function that combines
+their elements into one array without any repetition. 
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+//Set
 function mergeArrays(...arrays) {
-
     let jointArray = []
-    
     arrays.forEach(array => {
         jointArray = [...jointArray, ...array]
     });
-
     return [...new Set([...jointArray])]
-
-    
 }
 
+//set with .from()
+function mergeArrays(...arrays) {
+    let jointArray = []
+
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    });
+    return Array.from(new Set([...jointArray]))
+}
+
+//filter()
+function mergeArrays(...arrays) {
+    let jointArray = []
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    });
+    const uniqueArray = jointArray.filter((item, index) => jointArray.indexOf(item) === item)
+    return uniqueArray
+}
+
+//reduce
+function mergeArrays(...arrays) {
+    let jointArray = []
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    });
+    const uniqueArray = jointArray.reduce((newArray, item) => {
+        if (newArray.includes(item)) {
+            return newArray
+        } else return [...newArray, item]
+    }, [])
+    return uniqueArray
+}
 
 module.exports = mergeArrays
